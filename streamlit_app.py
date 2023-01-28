12d8b0503b96f56c75b242c1a9e8e844bfdcc8d9
Kahin Akram, Ph.D <74140556+KahinAkramHassan@users.noreply.github.com>
2023-01-28 23:36:43 +0100

    Update streamlit_app.py
    Added some additional text

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,9 @@
 import streamlit
 
 streamlit.title('Healthy New diner!')
+
+streamlit.header('Breakfast Menu')
+
+streamlit.text('Omege 3 & Blueberry oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boild Free-Range Egg')
