91ebb12c0566ef39094db8da62a9a5a20e62a34f
Kahin Akram, Ph.D <74140556+KahinAkramHassan@users.noreply.github.com>
2023-02-03 10:20:04 +0100

    Update streamlit_app.py
    Connecting the streamlit add to the snowflake table

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,3 +50,6 @@ streamlit.dataframe(my_data_rows)
 streamlit.header('What fruit would you like to add?')
 added_fruit = streamlit.text_input('What fruit would you like to add?', 'kiwi')
 streamlit.write('Thanks for adding: ', added_fruit)
+
+#will not work for now..
+my_cur.execute("insert into fruit_load_list values('from streamlist')")
